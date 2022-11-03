82a2d77a577382b9f3454cc95e4034ea1d984dea
SergePauli <sergepauli@yandex.ru>
2022-11-03 08:30:48 +0800

    feature: add data_set functional to AplicationRecord and test

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,31 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def data_sets
+    [:head, :item, :card, :summary, :full]
+  end
+
+  def custom_data(data_set)
+    if data_sets.include?(data_set)
+      method = method(data_set)
+      return method.call if method
+    end
+    false
+  end
+
+  def head
+    name || to_s
+  end
+
+  def item
+    { id: id, name: head }
+  end
+
+  def summary
+    { created: created_at || nil, updated: updated_at || nil }
+  end
+
+  def card
+    { head: head, id: id, summary: { created: created_at || nil, updated: updated_at || nil } }
+  end
 end
--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -27,4 +27,34 @@ RSpec.describe Area, type: :model do
     areas(:region).destroy
     expect { areas(:region).reload }.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it "should return default custom_data :head" do
+    expect(areas(:city).custom_data :head).to eq areas(:city).name
+  end
+
+  it "should return default custom_data :item" do
+    data = areas(:city).custom_data :item
+    expect(data[:name]).to eq areas(:city).name
+    expect(data[:id]).to eq areas(:city).id
+    expect(data[:created_at]).to be_nil
+  end
+
+  it "should return default custom_data :summary" do
+    data = areas(:city).custom_data :summary
+    expect(data[:created].to_formatted_s(:short)).to eq areas(:city).created_at.to_formatted_s(:short)
+    expect(data[:updated].to_formatted_s(:short)).to eq areas(:city).updated_at.to_formatted_s(:short)
+    expect(data[:id]).to be_nil
+  end
+
+  it "should return default custom_data :card" do
+    data = areas(:city).custom_data :card
+    expect(data[:summary][:created].to_formatted_s(:short)).to eq areas(:city).created_at.to_formatted_s(:short)
+    expect(data[:summary][:updated].to_formatted_s(:short)).to eq areas(:city).updated_at.to_formatted_s(:short)
+    expect(data[:id]).to eq areas(:city).id
+    expect(data[:head]).to eq areas(:city).name
+  end
+
+  it "should return false for custom_data :none" do
+    expect(areas(:city).custom_data :none).to eq false
+  end
 end
