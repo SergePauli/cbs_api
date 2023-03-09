25642641e694214711feca43cc530369de55d761
SergePauli <sergepauli@yandex.ru>
2023-03-09 14:15:54 +0800

    feature: add Functionality to StagePerformer model

--- app/models/stage_performer.rb
+++ app/models/stage_performer.rb
@@ -1,2 +1,24 @@
-class StagePerformer < ApplicationRecord
+class StagePerformer < MutableData
+  # аудит изменений
+  include Auditable
+
+  belongs_to :stage
+  belongs_to :performer
+  validates_associated :stage
+  validates_associated :performer
+  validates :stage, uniqueness: { scope: [:stage, :performer] }
+  alias_attribute :state, :performer # для поддержки MutableData
+  accepts_nested_attributes_for :performer
+
+  def name
+    performer.name
+  end
+
+  def card
+    super.merge({ stage: stage.item, performer: performer.item })
+  end
+
+  def self.permitted_params
+    super | [:stage_id, :performer_id] | [performer_attributes: Performer.permitted_params]
+  end
 end
