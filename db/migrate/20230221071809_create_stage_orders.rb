40d9defc04990b4c02792dcbb72fa0a93f56a4b6
SergePauli <sergepauli@yandex.ru>
2023-03-16 14:00:03 +0800

    fix: stage_orders migration

--- db/migrate/20230221071809_create_stage_orders.rb
+++ db/migrate/20230221071809_create_stage_orders.rb
@@ -1,11 +1,11 @@
-# заказы поставки
+# заказы и поставки
 class CreateStageOrders < ActiveRecord::Migration[6.1]
   def up
     create_table :stage_orders do |t|
       t.references :stage, null: false, foreign_key: true, comment: "этап"
       t.references :isecurity_tool, null: false, foreign_key: true, comment: "СЗИ, товар"
-      t.references :status, null: false, foreign_key: true, comment: "статус поставки"
-      t.references :contragent, null: false, foreign_key: true, comment: "поставщик"
+      t.references :order_status, null: false, comment: "статус поставки"
+      t.references :organization, null: false, foreign_key: true, comment: "поставщик"
       t.float :amount, comment: "колличество"
       t.date :requested_at, comment: "дата запроса счета"
       t.string :order_number, comment: "счет на поставку"
@@ -13,6 +13,8 @@ class CreateStageOrders < ActiveRecord::Migration[6.1]
       t.date :payment_at, comment: "дата оплаты счета"
       t.date :received_at, comment: "дата прихода"
       t.string :description, comment: "примечание"
+      t.integer :priority, index: true, null: false, default: 0, comment: "порядок в списке"
+      t.boolean :used, null: false, default: true, comment: "признак использования"
 
       t.uuid :list_key, null: false, comment: "служебный ключ списка, для логгирования"
       t.timestamps
