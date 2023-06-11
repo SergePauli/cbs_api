3e972389b776b938635b53d4013c5be9b7e72252
SergePauli <sergepauli@yandex.ru>
2023-06-11 10:34:35 +0800

    fix: Singleable method ensure_is_single - use only for creation

--- app/models/concerns/singleable.rb
+++ app/models/concerns/singleable.rb
@@ -2,13 +2,14 @@ module Singleable
   extend ActiveSupport::Concern
 
   included do
-    before_validation :ensure_is_single, if: -> { used? }
+    before_validation :ensure_is_single, if: -> { used? }, on: :create
 
     def ensure_is_single
       return if self === nil
       another_used = self.class.where({ main_model_id: main_model_id, used: true })
-      another_used = another_used.where.not({ id: id }) if id
-      another_used.update({ used: false }) if another_used.first
+      return unless another_used
+      another_used = another_used.first
+      another_used.update({ used: false }) if !!another_used
     end
   end
 end
