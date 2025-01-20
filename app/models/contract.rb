b4d24086521279ed2ab0b6bbc6a2a2193b6fc1e0
Serge Pauli <sergepauli@ya.ru>
2025-01-20 11:03:30 +0800

    fix basement for contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -45,7 +45,11 @@ class Contract < ApplicationRecord
 
   # реализация для набора данных basement
   def basement
-    { id: id, contragent: contragent.item, task_kind: task_kind.item, cost: cost ? "%.2f" % cost : cost, governmental: governmental, signed_at: to_date_str(signed_at), deadline_at: to_date_str(deadline_at), closed_at: to_date_str(closed_at), external_number: external_number, revision: revision.nil? ? revisions[0].basement : revision.basement, status: status.item, region: contragent.real_addr.nil? ? nil : contragent.real_addr.address.area.item }
+    begin
+      { id: id, contragent: contragent.item, task_kind: task_kind.item, cost: cost ? "%.2f" % cost : cost, governmental: governmental, signed_at: to_date_str(signed_at), deadline_at: to_date_str(deadline_at), closed_at: to_date_str(closed_at), external_number: external_number, revision: revision.nil? ? revisions[0].basement : revision.basement, status: status.item, region: contragent.real_addr.nil? ? nil : contragent.real_addr.address.area.item }
+    rescue 
+      raise ActiveRecord::RecordNotFound, "Не найдена ревизия к #{name} или другая ошибка"
+    end   
   end
 
   # реализация для набора данных card
