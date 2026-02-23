19fe18c87ed27372ec8767ea1ff360ab3aeccb86
Serge Pauli <sergepauli@ya.ru>
2026-02-23 08:22:21 +0800

    Fix container API reachability and CORS defaults

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,8 +6,17 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  cors_allow_origin = ENV.fetch("CORS_ALLOW_ORIGIN", "http://localhost:3000")
-  cors_origins = cors_allow_origin.split(",").map(&:strip).reject(&:empty?)
+  cors_allow_origin = ENV.fetch(
+    "CORS_ALLOW_ORIGIN",
+    "http://localhost:3000,http://cbs:3000,/\\Ahttp:\\/\\/192\\.168\\.\\d+\\.\\d+:3000\\z/"
+  )
+  cors_origins = cors_allow_origin.split(",").map(&:strip).reject(&:empty?).map do |origin|
+    if origin.start_with?("/") && origin.end_with?("/")
+      Regexp.new(origin[1..-2])
+    else
+      origin
+    end
+  end
 
   allow do
     origins(*cors_origins)
